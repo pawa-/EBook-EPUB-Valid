83c486ab3c1e731e394d16592b6fde0f0ef1fac1
pawa <pawa@pawafuru.com>
2013-08-15 11:14:56 +0900

    initial commit

---
+++ lib/EBook/EPUB/Valid.pm
@@ -0,0 +1,32 @@
+package EBook::EPUB::Valid;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+EBook::EPUB::Valid -
+
+=head1 SYNOPSIS
+
+  use EBook::EPUB::Valid;
+
+=head1 DESCRIPTION
+
+EBook::EPUB::Valid is
+
+=head1 AUTHOR
+
+pawa E<lt>pawapawa@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
